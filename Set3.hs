b38089eba8c4f7ebb6302d3538e22c37b1a23be4
Leon Coto <leoncoto@gmail.com>
2016-03-16 20:07:23 +0000

    Cheat on the last one...

--- Set3.hs
+++ Set3.hs
@@ -8,15 +8,18 @@ import MCPrelude
 -- Generating combinations
 
 allPairs :: [a] -> [b] -> [(a, b)]
-allPairs _ [] = []
-allPairs [] _ = []
 allPairs (x:xs) ys = pairWith x ys ++ allPairs xs ys
+allPairs _ _ = []
 
-pairWith _ [] = []
 pairWith x (y:ys) = (x, y) : pairWith x ys
+pairWith _ _ = []
+
+allPairs' :: [a] -> [b] -> [(a, b)]
+allPairs' (x:xs) ys = map ((,) x) ys ++ allPairs' xs ys
+allPairs' _ _ = []
 
 --------------------------------------------------------------------------------
---Poker hands
+-- Poker hands
 
 data Card = Card Int String
 
@@ -29,21 +32,24 @@ allCards rs ss = map (uncurry makeCard) $ allPairs rs ss
 makeCard r s = Card r s
 
 --------------------------------------------------------------------------------
---Generalizing pairs and cards
+-- Generalizing pairs and cards
 
 allCombs :: (a -> b -> c) -> [a] -> [b] -> [c]
-allCombs _ [] _ = []
-allCombs _ _ [] = []
 allCombs f (x:xs) ys = combWith f x ys ++ allCombs f xs ys
+allCombs _ _ _ = []
 
-combWith _ _ [] = []
 combWith f x (y:ys) = f x y : combWith f x ys
+combWith _ _ _ = []
 
 allCards' :: [Int] -> [String] -> [Card]
 allCards' rs ss = allCombs makeCard rs ss
 
+allCombs' :: (a -> b -> c) -> [a] -> [b] -> [c]
+allCombs' f (x:xs) ys = map (f x) ys ++ allCombs' f xs ys
+allCombs' _ _ _ = []
+
 --------------------------------------------------------------------------------
---Combinations of three things
+-- Combinations of three things
 
 a = [1, 2]
 b = [3, 4]
@@ -62,17 +68,24 @@ r =
   ]
 
 allCombs3 :: (a -> b -> c -> d) -> [a] -> [b] -> [c] -> [d]
-allCombs3 _ [] _ _ = []
-allCombs3 _ _ [] _ = []
-allCombs3 _ _ _ [] = []
 allCombs3 f (x:xs) ys zs = step1 f x ys zs ++ allCombs3 f xs ys zs
+allCombs3 _ _ _ _ = []
 
-step1 _ _ [] _ = []
-step1 _ _ _ [] = []
 step1 f x (y:ys) zs = step2 f x y zs ++ step1 f x ys zs
+step1 _ _ _ _ = []
 
-step2 _ _ _ [] = []
 step2 f x y (z:zs) = f x y z : step2 f x y zs
+step2 _ _ _ _ = []
 
 --------------------------------------------------------------------------------
---Combinations of more things
+-- Combinations of more things
+-- The Cheat
+-- https://github.com/spearsem/learnmeahaskell/blob/master/monad_challenge_solutions/Set3.hs#L41
+
+combStep :: [a -> b] -> [a] -> [b]
+combStep [] _ = []
+combStep _ [] = []
+combStep (f:fs) xs = map f xs ++ (combStep fs xs)
+
+allCombs3' :: (a -> b -> c -> d) -> [a] -> [b] -> [c] -> [d]
+allCombs3' f xs ys zs = (map f xs) `combStep` ys `combStep` zs
